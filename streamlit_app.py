182ee32f525f7d96f1cfffc91a5a7762d8a94ef5
Rohan Karan <rohankaran001@gmail.com>
2022-02-14 01:38:51 +0530

    update ui

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def main():
     tmdb_ak = os.getenv('TMDB_API_KEY', 'None')
     poster_path = 'https://api.themoviedb.org/3/find/{}?api_key=' + tmdb_ak + '&external_source=imdb_id'
 
-    movie = st.selectbox('Search for a movie', options=shuffle(f['primaryTitle'].to_numpy()), index=len(f) - 1)
+    movie = st.selectbox('Search for a movie', options=shuffle(f['primaryTitle'].to_numpy()))
     st.header("\n")
 
     # recommendation by getting value from similarity matrix
